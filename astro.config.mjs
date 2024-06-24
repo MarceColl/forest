9fcae9cd72f01ce865e9c78ccdc5f77c5676ad67
Marce Coll <marce@dziban.net>
2024-06-24 18:43:18 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
@@ -7,4 +9,8 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://dziban.net',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	}
 });
